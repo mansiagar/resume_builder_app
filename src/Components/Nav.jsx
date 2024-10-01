a203a1ced13b6d2cc1aeba9f7edcb690a53255d4
mansiagar <mansiagarwal293@gmail.com>
2024-10-01 12:13:54 +0530

    add logo

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,21 +2,13 @@ import React from "react";
 import { Menu } from "antd";
 import { Button } from "antd";
 import "./Nav.css";
-import logo from "../Assets/Logo.jpg";
+import logo from "../Assets/RESUME_LOGO.jpg";
 const Nav = () => {
   return (
     <Menu mode="horizontal" className="container">
       <div className="logo">
         <Menu.Item key="logo">
           <img src={logo} alt="Resume BUilder App" className="logo-img" />
-        </Menu.Item>
-      </div>
-      <div className="right-align">
-        <Menu.Item>
-          <Button className="btn">Sign Up</Button>
-        </Menu.Item>
-        <Menu.Item>
-          <Button className="btn">Login</Button>
         </Menu.Item>
       </div>
     </Menu>
